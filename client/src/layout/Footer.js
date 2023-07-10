87b07c2a8b393d0a90bf0fcff558719c7aa54f4b
Wangari Samantha <samanthanyams5@gmail.com>
2023-07-10 04:51:01 +0300

    components added

---
+++ client/src/layout/Footer.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+  return (
+    <footer className="fixed bottom-0 left-0 z-20 w-full p-4 bg-white border-t border-gray-200 shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 dark:border-gray-600">
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
+        © 2023 <a href="https://flowbite.com/" className="hover:underline">Drew's Planning™</a>. All Rights Reserved.
+      </span>
+      <ul className="flex flex-wrap items-center mt-3 text-sm font-medium text-gray-500 dark:text-gray-400 sm:mt-0">
+        <li>
+          <Link to="/about" className="mr-4 hover:underline md:mr-6">About</Link>
+        </li>
+        <li>
+          <Link to="/privacy_policy" className="mr-4 hover:underline md:mr-6">Privacy Policy</Link>
+        </li>
+        <li>
+          <Link to="/licensing" className="mr-4 hover:underline md:mr-6">Licensing</Link>
+        </li>
+        <li>
+          <Link to="/contact" className="hover:underline">Contact</Link>
+        </li>
+      </ul>
+    </footer>
+  );
+};
+
+export default Footer;
